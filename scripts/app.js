658312258251f27219cb0563f2e6d7f3ad581808
UnderdoneLizard <mitchelljmarrs@gmail.com>
2020-08-31 22:45:49 -0400

    lot more tsudocode

--- scripts/app.js
+++ scripts/app.js
@@ -7,10 +7,11 @@ const suits = ["d", "c", "h", "s"];
     //faceUp boolean
     //methods
         //constructor takes cards info sets face up to false 
-        //changeAceOne - when needed this will switch the ace value to one 
-        //changeAceBack - needs to be called if the first is, dont want to leave any aces at one (maybe dont need)
         //convertValue - uses object of values to convert 
+        //still maybe
         //flip card?
+        //I dont think I need either of these//changeAceOne - when needed this will switch the ace value to one 
+        //changeAceBack - needs to be called if the first is, dont want to leave any aces at one (maybe dont need)
 
 //deck class
     //cards array
@@ -18,8 +19,10 @@ const suits = ["d", "c", "h", "s"];
     //methods
         //constructor 
             //makes deck and depending on input will make multiple 
-        //remove card will return the card removed 
+        //remove card will checkCardsSize(>=1) and return the card removed 
         //add to discard 
+        //moveDisCards (unless i use a shuffle that don't need this)
+        //checkCardsSize - if empty moveDisCards shuffle
         //shuffle
         
 //player class
@@ -29,14 +32,45 @@ const suits = ["d", "c", "h", "s"];
     //chips/money - biggerNum
     //points - total value of current hand 
     //continue - boolean for the turns 
+    //hasAce - boolean
     //canDouble - boolean for if player can double down 
     //canSplit - boolean for if player can split 
+    //bust - boolean if the playes has lost the hand
+    //win - boolean dealt with in compair
+    //tie - boolean dealt with in compair
+    //lose - boolean dealt with in compair 
     //methods
-        //check - to see if the player has gone over and will do special stuff when an ace is in hand and will return new points if over changes continue to false 
+        //constructor
+            //takes name num(for money) sets all initial values doy
+        //check - to see if the player has gone over and will do special stuff when an ace is in hand and will return new points if over changes continue to false and bust to true
         //hit - adds a card to the player hand 
         //stand - changes continue to false 
-        //initialize round - sets player's continue to true, canDouble and canSplit to false, and calls each player to bet
-        //bet - players must bet minimum so maybe game has to be passed in otherwise will set player bet 
+        //bet - passed minimum bet, has the player select a bet, returns nothing  
+        //checkAce - checks for ace if so sets value to true
         //checkDoubleDown - looks at players hand to see if they can double down and sets value to true if so sets canDouble to true
         //doubleDown - double players bet hit once and set continue to false 
-        //
+        //split - will run play loop for player twice 
+        //win - adds double players bet value and adds to chips
+        //lose - delets players bet adds no money 
+        //tie - returns bet money 
+        //initializePlayer - sets player's continue to true, win, lose, bust, tie, hasAce, bust, canDouble and canSplit to false, and
+        //discard - will loop through hand and remove the cards at the end of the round
+
+//dealer class extends player
+    //constructor takes nothing 
+        //super infinant money and a rediculous name 
+    //deal - takes players array loops twice a loop that loops players cards into hand 
+    //check for dealer  
+
+
+//game class 
+    //minimum bet
+    //players = [array]
+    //methods
+        //constructor
+            //takes in number of players and number of decks 
+            //initialize round - calls initilizePlayer, calls playerBet for each player 
+            //compair - loop through players if bust is false compair to dealer, if highr win true, if lower lose true, if tie tie true. 
+            //distribute - loop through players if win double bet to player, if tie return bet to player, if bust or lose do nothing 
+            //playerPlay - while player continue is true offer to hit or stay 
+            //play round - initializeRound dealer deals and the players play (loop through the players) compair and distribute at the end 
